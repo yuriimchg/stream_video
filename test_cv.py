e16810bce7ca35327bd9c5fa6fe9444dcf89326b
YuriiMachuga <yurgan4@gmail.com>
2018-09-27 18:41:18 +0300

    Test webcam streaming

--- test_cv.py
+++ test_cv.py
@@ -4,8 +4,6 @@ cap = cv2.VideoCapture(0)
 while True:
     s, frame = cap.read()
     frame = cv2.flip(frame,1)
-    print(type(frame), len(frame))
-    #gray = cv2.cvtColor(frame, cv2.COLOR_BRG2GRAY)
     cv2.imshow('caption',frame)
     cv2.waitKey(1)
 
